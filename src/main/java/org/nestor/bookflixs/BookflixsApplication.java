7f9d21425e6bfb904c7fc6b476d9ae377acbbc9b
snownets <snownesst@gmail.com>
2017-11-26 17:18:21 -0600

    primer

---
+++ src/main/java/org/nestor/bookflixs/BookflixsApplication.java
@@ -0,0 +1,12 @@
+package org.nestor.bookflixs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookflixsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookflixsApplication.class, args);
+	}
+}
